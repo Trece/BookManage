316710c1f3abb294ba3ab34d9cb9cdb207316078
Winmad <winmad.wlf@gmail.com>
2013-11-09 19:54:09 +0800

    start writing :search_book feature

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ActiveRecord::Base
   attr_accessible :ISBN, :author, :description, :title, :total_num, :remain_num
   has_many :borrow_records
   has_many :borrowed_readers, through: :borrow_records, class_name: "Reader", source: :reader
+
+  def self.search(type, keywords)
+  end
 end
