8bea0b0b3c06d66f64f21b2ad32969c651ade8f0
Trece <treathen@hotmail.com>
2013-12-30 19:51:11 +0800

    Wang

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
   attr_accessible :jobid, :name
   has_one :reader
   def self.login_url
-    "http://127.0.0.1:3030/login/form/3284d43331f3c7ec02be2c9bcc7f9d18/0/users"
+    "http://fake-tsinghua-id.herokuapp.com/login/form/4b2437cf332ec565314c94163e3d93c5/0/users"
   end
   def self.appid
-    "bookmanage"
+    "book_manage"
   end
   def self.ticket_url
-    "http://127.0.0.1:3030/checkticket/" + self.appid + "/"
+    "http://fake-tsinghua-id.herokuapp.com/checkticket/" + self.appid + "/"
   end
   def is_admin?
     ["2010010001"].include? jobid
